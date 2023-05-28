fc39e0c8bf4c06b4240bff48fe0ca5a869347884
Guilherme <guilherme.gs17@outlook.com>
2023-05-27 21:41:41 -0300

    Ajustando controle dos filmes que aparecem na tela Home

---
+++ src/screens/Home/components/CardMovies/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native"
+
+
+export const styles = StyleSheet.create({
+    cardImage: {
+        width: 100,
+        height: 145,
+        borderRadius: 18,
+
+    },
+    cardMovies: {
+        width: 100,
+        height: 145,
+        borderRadius: 18,
+        marginHorizontal: 6,
+        marginBottom: 10,
+        backgroundColor: "#424242"
+    },
+})
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,15 +1,54 @@
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, FlatList } from "react-native";
 import { styles } from "./styles";
 import { MagnifyingGlass } from "phosphor-react-native";
+import { useEffect, useState } from "react";
+import { api } from "./services/api";
+import { CardMovies } from "./components/CardMovies";
+
+interface Movie {
+    id: number;
+    title: string;
+    poster_path: string;
+    overview: string;
+}
 
 export function Home() {
+    const [discoverMovies, setDiscoverMovies] = useState<Movie[]>([]);
+
+    useEffect(() => {
+        loadingMoreData();
+    }, []);
+
+    const loadingMoreData = async () => {
+        const response = await api.get("/movie/popular");
+        setDiscoverMovies(response.data.results);
+    };
+
     return (
         <View style={styles.container}>
-            <Text style={styles.headerText}>O que você quer ver hoje?</Text>
-            <View style={styles.containerInput}>
-                <TextInput placeholderTextColor="#FFF"
-                placeholder="Pesquisar" style={styles.input} />
-                <MagnifyingGlass color="#FFF" size={25} weight="light" />
+            <View style={styles.headerContent}>
+                <Text style={styles.headerText}>O que você quer ver hoje?</Text>
+                <View style={styles.containerInput}>
+
+                    <TextInput placeholderTextColor="#FFF"
+                        placeholder="Pesquisar" style={styles.input} />
+                    <MagnifyingGlass color="#FFF" size={25} weight="light" />
+                </View>
+            </View>
+
+            <View>
+                <FlatList
+                    data={discoverMovies}
+                    numColumns={3}
+                    renderItem={(item) =>
+                        <CardMovies data={item.item} />
+                    }
+                    showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{
+                        padding: 30,
+                        paddingBottom: 20,
+                    }}
+                />
             </View>
         </View>
     )
