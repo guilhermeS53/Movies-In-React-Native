7de67ea070d0e10d22b2927ad39d7abc0181240a
Guilherme <guilherme.gs17@outlook.com>
2023-05-29 20:32:15 -0300

    ..

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -4,6 +4,7 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#242A32',
+        overflow: "scroll",
     },
     headerContent: {
         padding: 20,
